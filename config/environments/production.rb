2bb3fcd466c03a8c2aae2c3b43bdb326c449a23f
inoue.yousuke.1991@gmail.com <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-06-30 13:00:52 +0000

    URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'afternoon-oasis-70388.herokuapp.com ' }
+  config.action_mailer.default_url_options = { host: 'http://52.192.46.121/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
