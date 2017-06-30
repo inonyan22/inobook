01d49ae7c06d404aa55a92b1a5ee025fe406b3a8
inoue.yousuke.1991@gmail.com <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-06-30 12:55:32 +0000

    メール修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
-   user_name: "調べたSENDGRID_USERNAME",
-   password: "調べたSENDGRID_PASSWORD",
-   domain: "heroku.com",
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "52.192.46.121",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
